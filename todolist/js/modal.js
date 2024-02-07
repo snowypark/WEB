f8d9abc9a8cd5de16266d782c3878264e4f97594
snowypark <partsnowy@gmail.com>
2024-02-07 15:21:04 +0900

    todoList

--- todolist/js/modal.js
+++ todolist/js/modal.js
@@ -1,3 +1,21 @@
+// window.onload = () => {
+//     const day = ["일", "월", "화", "수", "목", "금", "토"];
+//     let now = new Date();
+//     console.log(now.getFullYear());
+//     console.log(now.getMonth() +1);
+//     console.log(now.getDate());
+//     console.log(day[now.getDay()]);
+
+ 
+// }
+
+window.onkeyup = (e) => {
+    if(e.keyCode === 27) {
+        handleCancelClick();
+    }
+}
+
+
 function handleAddTodoModalOpen() {
     const modal = document.querySelector(".root-modal");
     const title = modal.querySelector(".modal-title");
@@ -6,13 +24,99 @@ function handleAddTodoModalOpen() {
     title.innerHTML = "추가하기";
     todoInput.value = "";
     submitButton.onclick = handleAddTodoSubmit;
+
+    todoInput.onkeydown = (e) => {
+        if(e.ctrlKey && e.keyCode ===13) {
+            submitButton.click();
+        }
+    }
+ 
+    modal.classList.add("modal-show");
+
+    
+}
+
+function handleEditTodoModalOpen(todoId) {
+    const modal = document.querySelector(".root-modal");
+    const title = modal.querySelector(".modal-title");
+    const todoInput = modal.querySelector(".todo-input");
+    const submitButton = modal.querySelector(".modal-button");
+    title.innerHTML = "수정하기";
+    
+    let todoListJson =  localStorage.getItem("todoList");
+    let todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    let findTodoByTodoId = todoList.filter(todo => todo.todoId === todoId)[0];
+
+    todoInput.value = findTodoByTodoId.content;
+    submitButton.onclick =() => handleEditTodoSubmit(todoId);
+
+    todoInput.onkeydown = (e) => {
+        if(e.ctrlKey && e.keyCode ===13) {
+            submitButton.click();
+        }
+    }
+    
     modal.classList.add("modal-show");
 
 }
 
+function convertDateKor(curruntDate) {
+    const daykors = ["일","월", "화", "수", "목", "금", "토"];
+    const year = curruntDate.getFullYear();
+    const month = curruntDate.getMonth() + 1;
+    const date = curruntDate.getDate();
+    const day = daykors[curruntDate.getDay()];
+
+    return `${year}년 ${month}월 ${date}일(${day})`;
+}
+
 function handleAddTodoSubmit() {
     const modal = document.querySelector(".root-modal");
+    const todoInput = modal.querySelector(".todo-input");
     modal.classList.remove("modal-show");
+
+    let todoListJson =  localStorage.getItem("todoList");
+    let todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    let lastTodoId = todoList.length === 0? 0 : todoList[todoList.length -1].todoId;
+    
+
+    let todoObject = {
+        todoId : lastTodoId+1,
+        content : todoInput.value,
+        date: convertDateKor(new Date())
+    }
+
+    todoList.push(todoObject);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function handleEditTodoSubmit(todoId) {
+    const modal = document.querySelector(".root-modal");
+    modal.classList.remove("modal-show");
+    let todoListJson =  localStorage.getItem("todoList");
+    let todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    let findIndex = -1;
+
+    for(let i = 0; i < todoList.length; i++) {
+        if(todoList[i].todoId === todoId) {
+            findIndex = i;
+            break;
+        }
+    }
+
+    if(findIndex === -1) {
+        alert("수정오류!");
+        return;
+    }
+
+    todoList[findIndex].content = document.querySelector(".todo-input").value;
+    todoList[findIndex].date = convertDateKor(new Date());
+
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    getTodoList();
 }
 
 function handleCancelClick() {
---
+++ todolist/js/todo.js
@@ -0,0 +1,61 @@
+window.onload = () => {
+    getTodoList();
+
+    const todoInput = document.querySelector(".todo-input");
+
+
+
+}
+
+function getTodoList() {
+    const todoContentList = document.querySelector(".todo-content-list");
+    const todoListJson = localStorage.getItem("todoList");
+    const todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    todoContentList.innerHTML = "";
+
+    for(let todo of todoList) {
+        todoContentList.innerHTML += `
+          <li class="todo-content-box">
+                    <div class="todo-content-header">
+                            <span>
+                                <i class="fa-regular fa-star"></i>
+                            </span>
+                            <span class="todo-content-date">
+                                ${todo.date}
+                            </span>
+                    </div>
+
+                    <div class="todo-content-main">
+                            <pre class="todo-content"> ${todo.content} </pre>
+                    </div>
+
+                    <div class="todo-content-footer">
+                            <button class="todo-edit-button" onclick="handleEditTodoModalOpen(${todo.todoId})">
+                                <i class="fa-solid fa-pencil"></i>
+                            </button>
+                            <button class="todo-remove-button" onclick="handleRemoveTodoClick(${todo.todoId})">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                     </div>
+
+            </li>
+        `;
+        
+    }
+
+}
+
+function handleRemoveTodoClick(todoId) {
+    let selected = confirm("정말로 삭제하시겠습니까?");
+    if(!selected) {
+        return;
+    }
+
+    const todoListJson = localStorage.getItem("todoList");
+    const todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+    const newTodoList = todoList.filter(todo => todo.todoId !== todoId);
+
+    localStorage.setItem("todoList", JSON.stringify(newTodoList));
+    getTodoList();
+}
